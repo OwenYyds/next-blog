4814b7ce6028b7775dca74d8f7bf7942a4ea06b2
Owen Yang <youngwhenball@gmail.com>
2025-02-22 17:45:21 +0800

    init redux

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,14 @@
 "use client";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+
+import { useAppDispatch } from "@/hooks/dispatch";
+import { fetchUserInfo } from "@/store/modules/userInfoStore";
+import { useEffect } from "react";
 
 export default function Header() {
-  const userInfo = useSelector(
-    (state: {
-      userInfo: {
-        name: string;
-        email: string;
-        phone: string;
-        address: string;
-        role: string;
-      };
-    }) => state.userInfo
-  );
-
-  const dispatch = useDispatch();
-
-  function setUserInfo() {
-    dispatch({
-      type: "userInfo/setUserInfo",
-      payload: {
-        name: "1112222",
-        email: "3333",
-        phone: "444",
-        address: "125553",
-        role: "admin",
-      },
-    });
-  }
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    console.log(userInfo);
-  }, [userInfo]);
-
-  return (
-    <div>
-      <h1>Header</h1>
-      <button onClick={setUserInfo}>Change User Info</button>
-    </div>
-  );
+    dispatch(fetchUserInfo());
+  }, [dispatch]);
+  return <div>test</div>;
 }
--- src/store/modules/userInfoStore.ts
+++ src/store/modules/userInfoStore.ts
@@ -1,15 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { AppDispatch } from "..";
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+type UserInfo = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+};
 
 const userInfoStore = createSlice({
   name: "userInfo",
   initialState: {
-    userInfo: {
-      name: "John Doe",
-      email: "eee",
-      phone: "123",
-      address: "123",
-      role: "user",
-    },
+    userInfo: [] as UserInfo[],
   },
   reducers: {
     setUserInfo(state, action) {
@@ -20,7 +43,13 @@ const userInfoStore = createSlice({
 
 const { setUserInfo } = userInfoStore.actions;
 
+const fetchUserInfo = () => async (dispatch: AppDispatch) => {
+  const response = await fetch("http://localhost:9876/0");
+  const data: UserInfo = await response.json();
+  dispatch(setUserInfo(data));
+};
+
 const reducers = userInfoStore.reducer;
 
-export { setUserInfo };
+export { setUserInfo, fetchUserInfo };
 export default reducers;
