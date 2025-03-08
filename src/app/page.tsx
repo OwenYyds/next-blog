830958d1b5794c74c7e1f3b9dedfafba3483f858
Owen Yang <youngwhenball@gmail.com>
2025-03-08 20:28:35 +0800

    add background image to Home component for enhanced visual appeal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import HomePage from "@/components/homePage/HomePage";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <div className="bg-[url(/bg.png)]">
+      <HomePage />
+    </div>
+  );
 }
