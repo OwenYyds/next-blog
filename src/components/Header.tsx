3785a1fca76e811a2f2cb5a513d758eed8e5dd0f
Owen Yang <youngwhenball@gmail.com>
2025-03-08 20:28:55 +0800

    refactor Header component for improved layout and add spacing to HomePage for better visual structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,28 +12,37 @@ import { GiNotebook } from "react-icons/gi";
 export default function Header() {
   const { isUserLoggedIn } = useUserInfo();
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand as={Link} href="#">
-        <Image src={brand} alt="blog Logo" width={50} height={50} />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Owen&apos;s Blog
-        </span>
-      </Navbar.Brand>
-      {isUserLoggedIn && <UserAvatar />}
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          Blog
-        </Navbar.Link>
-        <Navbar.Link href="#">Game</Navbar.Link>
-        <Navbar.Link href="#">Me</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-      <Button name="Write" icon={<GiNotebook />} duoTone="purpleToBlue" />
-      <DarkThemeToggle />
-      <Navbar.Toggle />
-    </Navbar>
+    <>
+      <Navbar
+        fluid
+        rounded
+        className="fixed w-full bg-opacity-20 backdrop-blur-md z-50"
+      >
+        <Navbar.Brand as={Link} href="#">
+          <Image src={brand} alt="blog Logo" width={50} height={50} />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Owen&apos;s Blog
+          </span>
+        </Navbar.Brand>
+        {isUserLoggedIn && <UserAvatar />}
+        <Navbar.Collapse>
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link as={Link} href="#">
+            Blog
+          </Navbar.Link>
+          <Navbar.Link href="#">Game</Navbar.Link>
+          <Navbar.Link href="#">Me</Navbar.Link>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+        <Button name="Write" icon={<GiNotebook />} duoTone="purpleToBlue" />
+        <DarkThemeToggle />
+        <Navbar.Toggle />
+      </Navbar>
+
+      {/* to make sure there's no blank space above nav */}
+      <div className="h-5 md:h-10 lg:h-12"></div>
+    </>
   );
 }
--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -4,7 +4,7 @@ import { ArticleCard } from "../ArticleCard";
 export default function HomePage() {
   return (
     <>
-      <div className="container my-5 text-center">
+      <div className="my-5 py-3 text-center">
         <div className="block text-5xl font-bold">Hello , i am Owen</div>
         <div className="block text-3xl font-semibold">Welcome to my blog</div>
         <div className="block">My blog is under construction</div>
