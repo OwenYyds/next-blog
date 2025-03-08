7aa9752809cc3e185bc6639ad8266a8c0fae4e32
Owen Yang <youngwhenball@gmail.com>
2025-03-08 13:46:21 +0800

    add error boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/store/Providers";
+import ErrorBoundary from "@/utils/ErrorBoundary";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <ErrorBoundary fallback={"something went wrong"}>
+            {children}
+          </ErrorBoundary>
+        </Providers>
       </body>
     </html>
   );
