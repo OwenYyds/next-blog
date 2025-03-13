59a980cbd7baf3219f8bb1cfb0f95b7f79ae9a79
Owen Yang <youngwhenball@gmail.com>
2025-03-13 11:36:10 +0800

    add ArticleCard component and integrate it into HomePage for enhanced content display

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { ArticleCard } from "../ArticleCard";
+'use client'
+import React from 'react'
+import ArticleCard from '../blog/ArticleCard'
 
 export default function HomePage() {
   return (
     <>
-      <div className="my-5 py-3 text-center">
+      <div className="my-5 py-3 h-screen flex flex-col justify-center items-center text-center">
         <div className="block text-5xl font-bold">Hello , i am Owen</div>
         <div className="block text-3xl font-semibold">Welcome to my blog</div>
         <div className="block">My blog is under construction</div>
       </div>
+
+      <hr className="my-5" />
 
       <div className="mx-auto max-w-screen-lg">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -18,5 +21,5 @@ export default function HomePage() {
         </div>
       </div>
     </>
-  );
+  )
 }
