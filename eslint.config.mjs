89b8693928185309820c95610525036d1796193b
Owen Yang <youngwhenball@gmail.com>
2025-03-13 12:11:50 +0800

    refactor ESLint configuration for improved readability and consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // 禁用强制使用分号
-      semi: ["error", "never"],
-      // 如果需要在导入语句后强制不使用分号
-      "import/no-duplicates": ["error", { considerQueryString: true }],
-    },
-  },
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+]
 
-export default eslintConfig;
+export default eslintConfig
